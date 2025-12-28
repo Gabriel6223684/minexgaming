ba62c8f83dbd454fb9218bbbb86ae6df74b267d7
Gabriel <gabrielgb.luiz000@gmail.com>
2025-12-28 18:35:55 -0400

    savecomplex

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,32 +1,24 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
 import "./global.css";
 import "./style.css";
-import "./tailwind.config";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { createRoot } from "react-dom/client";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import Explore from "./pages/Explore";
-import NotFound from "./pages/NotFound";
-import Projects from "./pages/Projects";
-import Profile from "./pages/Profile";
-import Messages from "./pages/Messages";
-import Index from "./pages/Index";
 
 const queryClient = new QueryClient();
 
+import { AuthProvider } from "./contexts/AuthContext";
+import { ThemeProvider } from "./contexts/ThemeContext";
+import { LanguageProvider } from "./contexts/LanguageContext";
+import Index from "./pages/Index";
+
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/explore" element={<Explore />} />
-        <Route path="/index" element={<Index />} />
-        <Route path="/messages" element={<Messages />} />
-        <Route path="/notfound" element={<NotFound />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
+    <ThemeProvider>
+      <LanguageProvider>
+        <AuthProvider>
+          <Index />
+        </AuthProvider>
+      </LanguageProvider>
+    </ThemeProvider>
   );
 }
-
-createRoot(document.getElementById("root")!).render(<App />);
--- vite-project/src/main.tsx
+++ vite-project/src/main.tsx
@@ -1,22 +1,23 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
 import "./global.css";
 import "./style.css";
-import "./tailwind.config";
-import Index from "./pages/Index";
+import "./theme.css";
 
-const queryClient = new QueryClient();
+import { ThemeProvider } from "./contexts/ThemeContext";
+import { ProfileProvider } from "./contexts/ProfileContext";
+import { LanguageProvider } from "./contexts/LanguageContext";
 
-export default function App() {
-  return (
-    <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/index" element={<Index />} />
-        </Routes>
-      </BrowserRouter>
-    </QueryClientProvider>
-  );
-}
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <ThemeProvider>
+      <LanguageProvider>
+        <ProfileProvider>
+          <App />
+        </ProfileProvider>
+      </LanguageProvider>
+    </ThemeProvider>
+  </React.StrictMode>
+);
