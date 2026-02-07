92ac62b95eb3e6fbbd0a283b2b4a49a186853835
group-networking <gabrielgb.luiz000@gmail.com>
2026-02-07 08:51:59 -0400

    comit

---
+++ minexgaming/src/components/LoginModal.tsx
@@ -0,0 +1,45 @@
+import "./Modal.css";
+import { useLanguage } from "../contexts/LanguageContext";
+
+type Props = {
+  onClose: () => void;
+  onOpenRegister?: () => void;
+};
+
+export default function LoginModal({ onClose, onOpenRegister }: Props) {
+  const { t } = useLanguage();
+
+  function handleSubmit(e: React.FormEvent) {
+    e.preventDefault();
+    alert("Login enviado (front-end)");
+  }
+
+  return (
+    <div className="modal-overlay">
+      <form className="modal" onSubmit={handleSubmit}>
+        <h2>{t.login.title}</h2>
+
+        <input placeholder={t.login.email} required />
+        <input placeholder={t.login.password} type="password" required />
+
+        <button className="submit" type="submit">
+          {t.login.submit}
+        </button>
+        <a
+          href="#"
+          className="modal-link"
+          onClick={(e) => {
+            e.preventDefault();
+            onOpenRegister?.();
+          }}
+        >
+          Não tem uma conta? Registre-se
+        </a><br></br>
+        <a href="#">Esqueceu a senha</a>
+        <button type="button" className="close" onClick={onClose}>
+          {t.login.close}
+        </button>
+      </form>
+    </div>
+  );
+}
